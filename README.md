1e8aff7bea7a63c21ec7bbd8200885f89a11f363
Glitch (humane-ionized-catshark) <none>
2024-09-18 18:25:42 +0000

    🧪🔱 Updated with Glitch

--- README.md
+++
@@ -1,1 +0,0 @@
-chatroom is now finished and up! go to the site and type /Chatroom.html to get to the chatroom. the general channel is the one that will be used.
--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "Clyde ";
+
+// Run when client connects
+io.on("connection", (socket) => {
   socket.on("joinRoom", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
 
